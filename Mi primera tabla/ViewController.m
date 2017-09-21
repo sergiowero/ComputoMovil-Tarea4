a75935a001b582d33c035a7e4d172937ada3cc0c
VirtualBox <virtualbox@VirtualBoxs-iMac.local>
2017-09-20 17:46:44 -0700

    Table programming and cell adjustment

--- Mi primera tabla/ViewController.m
+++ Mi primera tabla/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "cellMainTable.h"
 
 @interface ViewController ()
+
+@property NSMutableArray *userNames;
+@property NSMutableArray *userAges;
+@property NSMutableArray *userImages;
 
 @end
 
@@ -17,6 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initController];
 }
 
 
@@ -25,5 +31,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)initController {
+    self.userNames = [[NSMutableArray alloc] initWithObjects: @"Arya Stark", @"Cersei Lannister", @"Daenerys Targaryen", @"Jon Snow", nil];
+    
+    self.userImages = [[NSMutableArray alloc] initWithObjects: @"arya.jpg", @"cersei.jpg", @"daenerys.jpg", @"jon-snow.jpg", nil];
+    
+    self.userImages = [[NSMutableArray alloc] initWithObjects: @"16", @"42", @"30", @"31", nil];
+}
+
+#pragma mark - Table methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.userNames.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return 64;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    cellMainTable *cell = (cellMainTable *)[tableView dequeueReusableCellWithIdentifier: @"cellMainTable"];
+    
+    if(cell == nil)
+    {
+        [tableView registerNib:[UINib nibWithNibName: @"cellMainTable" bundle:nil] forCellReuseIdentifier: @"cellMainTable"];
+        cell = (cellMainTable *)[tableView dequeueReusableCellWithIdentifier: @"cellMainTable"];
+    }
+    
+    //cell.lblName.text = [self.userNames objectAtIndex:indexPath];
+    //cell.lblAge.text = [self.userAges objectAtIndex:indexPath];
+    //cell.imgUser.image = [self.userNames objectAtIndex:indexPath];
+    
+    return cell;
+    
+}
 
 @end
