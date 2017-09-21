03a27b4626d8f6ca76974106b00d04a150169dee
VirtualBox <virtualbox@VirtualBoxs-iMac.local>
2017-09-20 17:07:20 -0700

    Cell added and images

---
+++ Mi primera tabla/cellMainTable.h
@@ -0,0 +1,13 @@
+//
+//  cellMainTable.h
+//  Mi primera tabla
+//
+//  Created by VirtualBox on 9/20/17.
+//  Copyright © 2017 VirtualBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+
+@end
---
+++ Mi primera tabla/cellMainTable.m
@@ -0,0 +1,24 @@
+//
+//  cellMainTable.m
+//  Mi primera tabla
+//
+//  Created by VirtualBox on 9/20/17.
+//  Copyright © 2017 VirtualBox. All rights reserved.
+//
+
+#import "cellMainTable.h"
+
+@implementation cellMainTable
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
